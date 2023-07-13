e3456a8043ac2cbfc03b877d4362f4fe3a8b2c69
Janne57 <evgeniyahome1981@gmail.com>
2023-07-14 01:05:37 +0300

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes  from 'prop-types';
 import Notification from '../Notification/Notification.js';
 import css from './/Statistics.module.css';
